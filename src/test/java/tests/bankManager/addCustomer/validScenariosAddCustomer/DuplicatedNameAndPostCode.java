d6c66a104e11d850108dc105258bad3061e476f7
Alex-Matasa <alexandru.matasa86@gmail.com>
2025-01-04 12:48:21 +0200

    added duplicated data tests for Add Customer

---
+++ src/test/java/tests/bankManager/addCustomer/validScenariosAddCustomer/DuplicatedNameAndPostCode.java
@@ -0,0 +1,31 @@
+package tests.bankManager.addCustomer.validScenariosAddCustomer;
+
+import actions.BankManagerActions;
+import actions.LoginActions;
+import dataObjects.Customers;
+import dataObjects.DataModel;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import sharedData.Hooks;
+
+public class DuplicatedNameAndPostCode extends Hooks {
+
+    @Test
+    public void duplicatedNameAndPostCode() {
+        DataModel dataModel = new DataModel("src/test/resources/testData/addCustomer/validDataAddCustomer/duplicatedNameAndPostCode.json");
+        Customers customer1 = dataModel.customers.get(0);
+        Customers customer2 = dataModel.customers.get(1);
+        BankManagerActions bankManagerActions = new BankManagerActions(getDriver());
+        LoginActions loginActions = new LoginActions(getDriver());
+
+        loginActions.loginAsBankManager();
+        bankManagerActions.addCustomer(customer1);
+        dataModel.normalizeInputData(customer1);
+        bankManagerActions.addCustomer(customer2);
+        dataModel.normalizeInputData(customer2);
+        Assert.assertTrue(bankManagerActions.isCustomerInTheList(customer1));
+        Assert.assertTrue(bankManagerActions.isCustomerInTheList(customer2));
+    }
+
+
+}
